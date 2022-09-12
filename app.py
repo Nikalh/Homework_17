a4bfe65f9d1f27b1e21f360672070b58ca823bfa
Nikalh <nikalhov@gmail.com>
2022-09-12 20:44:43 +0300

    update app file with according to the comments

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # app.py
-import json
 from flask import Flask, request
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
@@ -25,35 +24,28 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
-# class MovieSchema(Schema):
-#     id = fields.Int()
-#     title = fields.Str()
-#     description = fields.Str()
-#     trailer = fields.Str()
-#     year = fields.Int()
-#     rating = fields.Float()
-#     genre_id = fields.Int()
-#     genre = fields.Str()
-#     director_id = fields.Int()
-#     director = fields.Str()
-
 
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+
+
 class DirectorSchema(Schema):
     id = fields.Int()
     name = fields.Str()
+
 
 class Genre(db.Model):
     __tablename__ = 'genre'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
 class GenreSchema(Schema):
     id = fields.Int()
     name = fields.Str()
+
 
 class MovieSchema(Schema):
     id = fields.Int()
@@ -62,10 +54,10 @@ class MovieSchema(Schema):
     trailer = fields.Str()
     year = fields.Int()
     rating = fields.Float()
-    #genre_id = fields.Int()
-    genre = fields.Pluck('GenreSchema','name')
-    #director_id = fields.Int()
-    director = fields.Pluck('DirectorSchema','name')
+    # genre_id = fields.Int()
+    genre = fields.Pluck('GenreSchema', 'name')
+    # director_id = fields.Int()
+    director = fields.Pluck('DirectorSchema', 'name')
 
 
 movie_schema = MovieSchema()
@@ -82,72 +74,74 @@ movie_ns = api.namespace('movies')
 director_ns = api.namespace('directors')
 genre_ns = api.namespace('genres')
 
-#Возвращаем список всех фильмов
+
+# Возвращаем список всех фильмов
 @movie_ns.route('/')
 class MovieView(Resource):
     def get(self):
-        result = Movie.query.all()
-        movies = movies_schema.dump(result)
-        return movies
+        result = Movie.query
+        #movies = movies_schema.dump(result)
+        director_id = request.args.get("director_id")
+        genre_id = request.args.get("genre_id")
 
-#Возвращаем подробную информацию о фильме
+        # Возвращаем только фильмы с определенным режиссером по запросу
+        if director_id :
+            result = result.filter(Movie.director_id == director_id)
+
+        # Возвращаем только фильмы с определенным жанром по запросу
+        if genre_id :
+            result = result.filter(Movie.genre_id == genre_id)
+        return movies_schema.dump(result.all()), 200
+
+    # Добавляем новый фильм
+    def post(self):
+        data = request.json
+        try:
+            db.session.add(Movie(**data))
+            db.session.commit()
+            return "Данные успешно добавлены", 201
+        except Exception as e:
+            print (e)
+            db.session.rollback()
+            return "Неудача", 500
+
+
+# Возвращаем подробную информацию о фильме
 @movie_ns.route('/<int:idx>')
 class MovieView(Resource):
     def get(self, idx):
         movie = Movie.query.get(idx)
         return movie_schema.dump(movie)
 
-#Возвращаем только фильмы с определенным режиссером по запросу
-@movie_ns.route('/director_id=<int:idx>')
-class MovieView(Resource):
-    def get(self, idx):
-
+    # Обновляем фильмы с определенным запросом
+    def put(self, idx):
+        movie = Movie.query.get(idx)
+        req_json = request.json
         try:
-            result = []
-            for movie in Movie.query.all():
-                #
-                if idx == movie.director_id:
-                    result.append(movie)
-
-            return movies_schema.dump(result), 200
+            movie.title  = req_json.get("title")
+            movie.description   = req_json.get("description")
+            movie.trailer = req_json.get("trailer")
+            movie.year = req_json.get('year')
+            movie.rating = req_json.get('rating')
+            movie.genre_id = req_json.get('genre_id')
+            movie.director_id = req_json.get('director_id')
+            db.session.add(movie)
+            db.session.commit()
+            return "Данные обновлены", 204
         except Exception as e:
-            return '', 404
+            print(e)
+            db.session.rollback()
+            return "Неудачное обновление", 500
 
-#Возвращаем только фильмы с определенным режиссером и жанром по запросу
-@movie_ns.route('/director_id=<int:idx>&genre_id=<int:uid>')
-class MovieView(Resource):
-    def get(self, idx, uid):
-        try:
+    # Удаляем только фильм с определенным запросом
+    def delete(self,idx):
+        movie = Movie.query.get(idx)
+        db.session.delete(movie)
+        db.session.commit()
+        return "Данные удалены", 204
 
-            result = []
-            for movie in Movie.query.all():
-                #
-                if idx == movie.director_id and uid == movie.genre_id:
-                    result.append(movie)
-                    #print(result)
 
-            return movies_schema.dump(result), 200
-        except Exception as e:
-            return '', 404
-
-#Возвращаем только фильмы с определенным жанром по запросу
-@movie_ns.route('/genre_id=<int:idx>')
-class MovieView(Resource):
-    def get(self, idx):
-        try:
-
-            result = []
-            for movie in Movie.query.all():
-                #
-                if idx == movie.genre_id:
-                    result.append(movie)
-                    #print(result)
-
-            return movies_schema.dump(result), 200
-        except Exception as e:
-            return '', 404
-
-#Возвращаем информацию о режиссерах
+# Возвращаем информацию о режиссерах
 @director_ns.route('/')
 class DirectorView(Resource):
     def get(self):
@@ -155,14 +149,49 @@ class DirectorView(Resource):
         directors = directors_schema.dump(result)
         return directors
 
-#Возвращаем только подробную информацию о режиссере
+    # Добавляем нового режиссера
+    def post(self):
+        data = request.json
+        try:
+            db.session.add(Director(**data))
+            db.session.commit()
+            return "Данные успешно добавлены", 201
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return "Неудача", 500
+
+# Возвращаем только подробную информацию о режиссере
 @director_ns.route('/<int:idx>')
 class DirectorView(Resource):
     def get(self, idx):
         director = Director.query.get(idx)
         return director_schema.dump(director)
 
-#Возвращаем все жанры
+    # Обновляем данные режиссера с определенным запросом
+    def put(self, idx):
+        movie = Director.query.get(idx)
+        req_json = request.json
+        try:
+            movie.name  = req_json.get("name")
+            db.session.add(movie)
+            db.session.commit()
+            return "Данные обновлены", 204
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return "Неудачное обновление", 500
+
+    # Удаляем режиссера с определенным запросом
+    def delete(self,idx):
+        movie = Director.query.get(idx)
+        db.session.delete(movie)
+        db.session.commit()
+        return "Данные удалены", 204
+
+
+
+# Возвращаем все жанры
 @genre_ns.route('/')
 class GenreView(Resource):
     def get(self):
@@ -170,7 +199,19 @@ class GenreView(Resource):
         genres = genres_schema.dump(result)
         return genres
 
-#Возвращаем только информацию о жанре с перечислением списка фильмов по жанру
+        # Добавляем новый жанр
+    def post(self):
+        data = request.json
+        try:
+            db.session.add(Genre(**data))
+            db.session.commit()
+            return "Данные успешно добавлены", 201
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return "Неудача", 500
+
+# Возвращаем только информацию о жанре с перечислением списка фильмов по жанру
 @genre_ns.route('/<int:idx>')
 class GenreView(Resource):
     def get(self, idx):
@@ -183,10 +224,30 @@ class GenreView(Resource):
                 if genre.id == movie.genre_id:
                     result.append(movie)
 
-
             return movies_schema.dump(result), 200
         except Exception as e:
             return e, 404
+
+    # Обновляем данные жанра с определенным запросом
+    def put(self, idx):
+        movie = Director.query.get(idx)
+        req_json = request.json
+        try:
+            movie.name  = req_json.get("name")
+            db.session.add(movie)
+            db.session.commit()
+            return "Данные обновлены", 204
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return "Неудачное обновление", 500
+
+    # Удаляем жанр с определенным запросом
+    def delete(self,idx):
+        movie = Director.query.get(idx)
+        db.session.delete(movie)
+        db.session.commit()
+        return "Данные удалены", 204
 
 
 if __name__ == '__main__':
